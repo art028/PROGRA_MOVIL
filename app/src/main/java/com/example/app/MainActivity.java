cbfba041e280a5b7521baec40f148f2bb01f99d4
art028 <120035338@upq.edu.mx>
2023-05-31 22:58:05 -0600

    Nuevos cambios de la base de datos

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -10,27 +10,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edtCodigo,edtNombre,edtCantidad;
-    Button btnAgregar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtCodigo=(EditText)findViewById(R.id.edtCodigo);
-        edtNombre=(EditText)findViewById(R.id.edtNombre);
-        edtCantidad=(EditText)findViewById(R.id.edtCantidad);
-        btnAgregar=(Button)findViewById(R.id.btnAgregar);
 
-        final claseBD claseBD=new claseBD(getApplicationContext());
-
-        btnAgregar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                claseBD.agregarProd(edtCodigo.getText().toString(),edtNombre.getText().toString(), Integer.valueOf(edtCantidad.getText().toString()));
-                Toast.makeText(getApplicationContext(),"Se Agrego Correctamente",Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
